ef182e9355f9336e64a635350a4dac06d652d03a
bartok.bence <bartok.bence@ckik.loc>
2024-12-19 11:51:45 +0100

    Második feladat

--- IKTProjekt_BB_SzK.py
+++ IKTProjekt_BB_SzK.py
@@ -16,3 +16,16 @@ for i in range(n):
         break
     if not fennsík:
         print("a) Nincs fennsík az adatok között.")
+
+# b) Melyik két pont között volt a legkisebb emelkedési szög?
+min_difference = float('inf') # Nagyon nagy szám a kezdéshez
+min_index = -1
+for i in range(n - 1):
+    difference = t[i + 1] - t[i]
+    if difference > 0 and difference < min_difference: # Csak emelkedést nézünk
+        min_difference = difference
+        min_index = i
+    if min_index != -1:
+        print(f"b) Az {min_index + 1}. mérési pont volt a legkisebb emelkedési szögű.")
+    else:
+        print("b) Nem volt emelkedés a pontok között.")
