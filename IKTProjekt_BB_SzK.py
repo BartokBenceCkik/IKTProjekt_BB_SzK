0e0a1807f0a11f0987af37ac4e057252eb48558e
bartok.bence <bartok.bence@ckik.loc>
2025-01-09 11:33:28 +0100

    Harmadik feladat

--- IKTProjekt_BB_SzK.py
+++ IKTProjekt_BB_SzK.py
@@ -1,17 +1,17 @@
-# Bartók Bence, Szabó Kornél, 10.b
+# Bartók Bence 10.b
 #Python első beadandó
 
 import math
 
 # Bemenet bekérése
 n = int(input("Add meg a mérési pontok számát (n): "))
-t = list(map(int, input("Add meg a mért magasságokat (szóközzel elválasztva): ").split()))
+t = [int(x) for x in input("Add meg a mért magasságokat (szóközzel elválasztva): ").split()]
 
 # a) Van-e a mért adatok között fennsík?
 fennsík = False
 for i in range(n):
     if t[i] >= 500:
-        print(f"a) Az {i + 1}. mérési pontnál már fennsíkon vagyunk.")
+        print("a) Az {}. mérési pontnál már fennsíkon vagyunk.".format(i + 1))
         fennsík = True
         break
     if not fennsík:
@@ -29,3 +29,16 @@ for i in range(n - 1):
         print(f"b) Az {min_index + 1}. mérési pont volt a legkisebb emelkedési szögű.")
     else:
         print("b) Nem volt emelkedés a pontok között.")
+
+# c) Érintett-e a túra során nyerget?
+nyereg_van = False
+
+for i in range(1, n):
+    if t[i] == t[i - 1]:
+        nyereg_van = True
+        break
+
+if nyereg_van:
+    print("c) Igen, érintett nyerget.")
+else:
+    print("c) Nem érintett nyerget.")
